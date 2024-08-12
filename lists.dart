ae905fe4e1bc5bb820941e7eb5659e2792105824
Temmy <abdulkareemtemitope01@gmail.com>
2024-08-12 08:00:03 +0100

    demo-dart commit

---
+++ lists.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> fruits = ["mango", "orange", "apple", "watermelon"];
+  print(fruits);
+  print(fruits[0]);
+  print(fruits[1]);
+  print(fruits[2]);
+  print(fruits[3]);
+}
---
+++ strings.dart
@@ -0,0 +1,13 @@
+void main() {
+  // strings are sets of paragrapghs/text in alphanumeric or alphabets
+
+  String name = "Abdulkareem";
+  String phonenumber = "25";
+  String city = "Ilorin";
+  String occupation = "Software Developer";
+
+  print(name);
+  print(phonenumber);
+  print(city);
+  print(occupation);
+}
